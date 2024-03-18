ac286d92e6089a8162ec380fa0b68b8c9176352c
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-03-18 22:21:49 +0530

    Complited Operators Lesson

---
+++ Operators/script.js
@@ -0,0 +1,79 @@
+/*
+Javascript operators are used to perform different types of mathematical and logical computations.
+*/ 
+
+// Arithmetic Operators
+
+/*let x= 5,y=6;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x**y);
+console.log(++x);
+console.log(y--);
+console.log(x%y);
+*/
+
+// Assignment Operators
+
+/*
+let x=2,y=4;
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x**=y);
+console.log(x%=y);
+*/
+
+
+// Logical Assignment Operators
+
+/*
+
+let x=24;
+
+if(x>18&&x<30){
+    console.log("You are young");
+}else if(x<10||x>30){
+    console.log("I think you are baby or vary old");
+}
+
+else if (x!=18){
+    console.log(`you are not young`)
+}
+
+*/
+
+// Comparision Operators
+
+/*
+***********************
+*/
+
+
+// Home Works
+
+// Arithmetic Operators
+let x=4,y=5;
+
+/*
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(++x);
+console.log(--y);
+*/
+
+
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);;
+console.log(x!==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x<=y);
+console.log(x>=y);
