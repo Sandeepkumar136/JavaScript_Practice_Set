19e8f5ab61e93a3bc43266553c644874ef225e58
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-03-22 23:29:46 +0530

    complited array search mathod

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,5 +1,6 @@
 // Arrays
 
+
 /*
 
 
@@ -49,7 +50,7 @@ console.log(x.pop());
     const x=[85,87,54,25,34,16];
     x.pop();
     console.log(x);
-}
+};
 
 
 /*
@@ -62,7 +63,7 @@ The push() method adds a new element to an array (at the end):
     x.pop();
     x.push(136)
     console.log(x);
-}
+};
 
 
 
@@ -82,7 +83,7 @@ The push() method adds a new element to an array (at the end):
     user_name.unshift("Rahul kumar")
     console.log(user_name);
 
- }
+ };
 
 
  /* 
@@ -95,7 +96,7 @@ The push() method adds a new element to an array (at the end):
 
     Roll_number[2]=284;
     console.log(Roll_number[2]);
- }
+ };
 
 
  /* 
@@ -107,7 +108,7 @@ The push() method adds a new element to an array (at the end):
     let amounts=[12,54,25,17,19,25,48,54,7,85];
     amounts[amounts.length]=99;
     console.log(amounts);
- }
+ };
 
 
 // NOTE: Using delete() leaves undefined holes in the array.
@@ -122,7 +123,7 @@ The concat() method creates a new array by merging (concatenating) existing arra
     const student_name_section_B=['Rockey','Ganga Ram', 'Mahavir'];
     const student_name_in_all_class=student_name_section_A.concat(student_name_section_B);
     console.log(student_name_in_all_class);
-}
+};
          //Merging Multible Arrays
 
 {
@@ -130,7 +131,7 @@ The concat() method creates a new array by merging (concatenating) existing arra
     const classes2=[1,2,3,4,5,6,7,8,9];
     const classes3=[10,11,12];
     console.log(classes.concat(classes2,classes3)) ;
-}
+};
 
 
 /* 
@@ -145,7 +146,7 @@ copyWithin() copies array elements to another position in an array, overwriting 
 
     courses.copyWithin(3,0);
     console.log(courses);
-}
+};
 
 
 /*
@@ -166,7 +167,7 @@ The flat() method creates a new array with sub-array elements concatenated to a 
 {
     const multipleArr=[[12,45,87],[48,75,15,17],[78,25,34,16,85]];
     console.log(multipleArr.flat())
-}
+};
 
 
 /*
@@ -182,4 +183,116 @@ Splicing and Slicing Arrays
             const Multible_of_Five=[5,10,15,20,25,30,35,40,45,50];
             Multible_of_Five.splice(0,1,2,4);
             console.log(Multible_of_Five);
+        };
+
+
+        /*
+        If the end argument is omitted, like in the first examples, the slice() method slices out the rest of the array.
+         */
+
+
+        {
+            const Multible_of_Two=[2,4,6,8,10,12,14,16,18,20];
+            let Config=Multible_of_Two.slice(4);
+            console.log(Config);
+        };
+// NOTE:  slice() method doesn't modify the original array, it returns a new array with the specified portion of the original array.
+
+
+                //   SEARCH MATHOD
+
+    /**
+     * Note: The first item has position 0, the second item has position 1, and so on.
+     */
+
+    // The indexOf() method searches an array for an element value and returns its position.
+
+
+    {
+        const Student_name=["Sandeep Kumar", 'Rahul kumar', 'Ramzan Ali', 'Ram kumar', 'Krishna kumar'];
+        let config=Student_name.indexOf('Ram kumar');
+        console.log("Which i searched in array is position is "+config);
+    };
+    
+
+    // Array.lastIndexOf() is the same as Array.indexOf(), but returns the position of the last occurrence of the specified element.
+
+
+    {
+        const Student_name=["Sandeep Kumar", 'Rahul kumar', 'Ramzan ALi', 'Ram kumar', 'Krishna kumar','Raunak kumar','Ramzan ALi'];
+        let config=Student_name.lastIndexOf("Ramzan ALi");
+        console.log("Which i searched of last occurrence in array "+config);
+    };
+
+
+    /*
+    ECMAScript 2016 introduced Array.includes() to arrays. This allows us to check if an element is present in an array (including NaN, unlike indexOf).
+     */
+
+    {
+        const passes_out_student=[256,148,234,125,856,145,264,125];
+        console.log(passes_out_student.includes(125));
+    };
+
+
+    /* JAVASCRIPT ARRAY find()*/
+    {
+        const invoice_numbers=[10,12,15,17,16,14,21];
+
+        function Find_Index_Value(value, index, array){
+            return value>20;
         }
+        const shortlisted_invoice_number=invoice_numbers.find(Find_Index_Value);
+
+        console.log(shortlisted_invoice_number);
+    };
+
+    
+
+    /**
+     * NOTE: that the function takes 3 arguments:
+     * The item value
+     * The item index
+     * The array itself
+     */
+
+    {
+        const doc_numbers=[241,548,241,654,874,124,124,121];
+        console.log(doc_numbers.find(function(value, index, array){return value< 242}));
+    };
+
+    {
+        const vol_number=[241,215,184,162,245,184,352,146,751,245,154];
+        console.log(vol_number.find(myfunction=(value, index)=>{return value<147}));
+    };
+
+    /*ES2023 added the findLast() method that will start from the end of an array and return the value of the first element that satisfies a condition.*/
+
+
+    {
+        const celcius=[12,14,15,16,24,12,20,24];
+        console.log(celcius.findIndex(value=>value>17));
+    };
+
+
+    /*
+    SORTING AN ARRAY
+    */
+
+    // The sort() method sorts an array alphabetically:
+
+    {
+        const Student_name=['Sandeep Kumar','Shoquat imam', 'Rajat Sarkar', 'Kishan Singh', 'Ravi kumar saha', 'pappu kumar', 'Saurav Sharma'];
+
+        Student_name.sort();
+m
+        console.log(Student_name);
+        
+    }
+
+
+    // The reverse() method reverses the elements in an array:
+
+    {
+        
+    }
