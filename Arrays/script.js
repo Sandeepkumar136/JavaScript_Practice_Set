f0f9e0f1d7da66a235766c2ac57924049fdf7b2e
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-03-24 22:21:12 +0530

    Completed Weekend Task

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,6 +1,7 @@
 // Arrays
 
 
+
 /*
 
 
@@ -285,14 +286,205 @@ Splicing and Slicing Arrays
         const Student_name=['Sandeep Kumar','Shoquat imam', 'Rajat Sarkar', 'Kishan Singh', 'Ravi kumar saha', 'pappu kumar', 'Saurav Sharma'];
 
         Student_name.sort();
-m
+
         console.log(Student_name);
         
-    }
+    };
 
 
     // The reverse() method reverses the elements in an array:
 
     {
+        const numbers=[1,2,23,4,8,65,14,75,974];
+        numbers.reverse();
+        console.log(numbers)
+    };
+
+
+    /**
+     * By combining sort() and reverse(), you can sort an array in descending order:
+     */
+
+    {
+        const failed_students=['hasan ali', 'ramzan ali', 'bishal saha', 'Raman kumar'];
+        console.log(failed_students.sort().reverse());
+    };
+
+
+    // ES2023 added the toSorted() method as a safe way to sort an array without altering the original array.
+
+    {
+        const contents=['Sandeep kumar', 'arvind kumar', 'ravi kumar', 'subodh gupta', 'nazim ali'];
+        const New_contents=contents.toSorted();
+        console.log(New_contents);
+    };
+    /**
+     * NOTE: This feature is not working in ms edge 2024 version march this mathod name is toSorted and toReverse
+     */
+
+
+
+
+
+    /**
+     * If numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+     * Because of this, the sort() method will produce incorrect result when sorting numbers.
+     */
+
+
+
+    {
+        const num=[2,4,6,8,10,12,14,16,18,20];
+        console.log(num.sort((a, b)=> b-a));
         
+    };
+
+    {
+        const num=[50,45,40,35,30,25,20,15,10,5];
+
+        num.forEach((index)=>{
+            console.log(index)
+        })
+
+        console.log(num.sort(function myfunction(a,b){
+           return  a-b
+        }));
+
+        console.log(num);
+    };
+
+
+
+    // Using a sort function, like explained above, you can sort an numeric array in random order
+    {
+        const num=[5,1,4,10,12,24,35,21,74,16,25,46,24,17,19];
+        console.log(num.sort(() =>  0.5-Math.random()));
+
+    };
+
+
+
+    /**
+     * THE FISHER YATES METHOD
+     */
+
+    /*
+    The points.sort() method in the example above is not accurate. It will favor some numbers over others.
+
+    The most popular correct method, is called the Fisher Yates shuffle, and was introduced in data science as early as 1938!
+
+    In JavaScript the method can be translated to this:
+     */
+
+    {
+
+        const num=[3,6,9,12,15,18,21,24,27,30];
+        for(let i=num.length -1; i>0; i--){
+            let x=Math.floor(Math.random()*(i+1));
+            let y=num[i];
+
+            num[i]=num[x];
+            num[x]=y;
+            
+        }
+        console.log(num)
+    };
+
+
+
+    /**
+     * WEEKEND WORKS
+     */
+    // Sort an array of numbers in ascending order.
+
+    {
+        const numbers=[2,4,5,1,6,7,9,2,6,4,2,3,4];
+        console.log(numbers.sort((a,b)=>a-b));
     }
+    // Sort an array of words in alphabetical order.
+    {
+        const words=['doing', 'coming', 'youtube', 'instagram', 'whatsapp'];
+        console.log(words.sort());
+    };
+
+    // Sort an array of ages in descending order.
+    {
+        let Ages=[12,52,13,10,17,18,19,15,18,16,14];
+        function myAgeFun(a,b){
+            return b-a;
+        }
+        console.log(Ages.sort(myAgeFun));
+    };
+
+
+    // Find if a specific number exists in an array of numbers.
+    {
+        function myTaskfunction(){
+            let numpy=[5,8,6,1,7,9,2,4,15,46];
+            console.log(numpy.indexOf(2));
+        }
+        myTaskfunction()
+    }
+
+
+    // Search for a specific word in an array of words.
+
+    {
+        const user_names=['Raman jha', 'ganga parsad', 'ashok gupta', 'Sandeep kumar'];
+        console.log(user_names.indexOf('Sandeep kumar')+1);
+    };
+
+    // Calculate the sum of all numbers in an array.
+    {
+        function MySumFraction(numbers){
+        let total =0;
+        for(let number of numbers){
+            total += number;
+        }
+        return total;
+    }
+    const amounts=[25,14,16,18,24,16,14,10,12,18,19];
+    console.log(MySumFraction(amounts));
+    };
+
+    // Remove any duplicate numbers from an array.
+    {
+        function myDupifunction(numpy){
+            let duplinum=[];
+            for(let number of numpy){
+                if(!duplinum.includes(number)){
+                    duplinum.push(number)
+                }
+            }
+            return duplinum;
+        }
+        let duplicatenums=[2,1,9,4,6,9,7,2,1,5,4,6];
+        function MyAssendFuntion(a,b){
+            return a-b;
+        }
+        let result=duplicatenums.sort(MyAssendFuntion);
+        console.log(myDupifunction(duplicatenums))
+    };
+
+    // Check if all elements in an array are even numbers.
+
+    {
+        function EvenCheckingFunction(Array_Name){
+            let result=[];
+            for(let i in Array_Name){
+                if (i % 2===0){
+                    result.push("This is Even Number")
+                }else{
+                    result.push('This is Odd Number')
+                }
+            }
+            return result;
+        }
+        let Arr_One=[2,4,6,8,10,20];
+        let Arr_Two=[1,2,3,4,5,6,7,8,9,10];
+        console.log(EvenCheckingFunction(Arr_One).concat(EvenCheckingFunction(Arr_Two)));
+    };
+
+    /**
+     * WeekEnd Task Completed
+     */
